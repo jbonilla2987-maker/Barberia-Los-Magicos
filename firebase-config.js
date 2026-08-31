71cddd4cfce57086930b7a826abd06b66c751f2d
jbonilla2987-maker <jbonilla2987@gmail.com>
2026-08-31 14:10:53 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCNngjdKmnaw2Bb00oq1B8tgfDLgc6bngo",
+  apiKey: "AIzaSyCNngjdKmnaW2BbOOoq1B8tgfDLgc6bngo",
   authDomain: "barberialosmagicos.firebaseapp.com",
   projectId: "barberialosmagicos",
   storageBucket: "barberialosmagicos.firebasestorage.app",
